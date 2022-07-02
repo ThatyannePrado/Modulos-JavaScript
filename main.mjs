ebcb857ec6c27302af050731ecf84c9cf0df1a6c
ThatyannePrado <thatyanneprado@poli.ufrj.br>
2022-07-02 01:29:23 -0300

    commit inicial

---
+++ main.mjs
@@ -0,0 +1,9 @@
+import {
+    mostraCidade,
+    mostraHobby,
+    mostraIdade,
+ } from './funcoes.mjs';
+
+ console.log(mostraIdade('Ana', 14));
+ console.log(mostraCidade('Ana', 'Mossoró'));
+ console.log(mostraHobby('Ana', 'rugby'));
